b9a55ce625f2b7a47979a80b29e18332b0a3ab52
Sheref Sidarous <sherefyounan@gmail.com>
2023-07-21 07:51:22 -0700

    cleaned up build_and_run.sh

--- FreeRTOS-Qemu-Demo/build_and_run.sh
+++ FreeRTOS-Qemu-Demo/build_and_run.sh
@@ -1,17 +1,17 @@
 #! /bin/sh
 set -e
 
-#rm -f librust_app.a
-#cp ../../../../rust-app/target/thumbv7m-none-eabi/debug/librust_app.a .
-#cp /home/shiro/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/thumbv7m-none-eabi/lib/libcore-*.rlib librust_core.a
-#cp /home/shiro/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/thumbv7m-none-eabi/lib/liballoc-*.rlib librust_alloc.a
-#cp /home/shiro/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/lib/rustlib/thumbv7m-none-eabi/lib/librustc_std_workspace_core-*.rlib librustc_std_workspace_core.a
+if [ $1 = "--debug" ]
+then
+DEBUG_MSG="Will halt and wait for gdb attach on port 1234"
+DEBUG_ARG="-S"
+else
+DEBUG_MSG="If you want to use gdb, call this script with --debug"
+DEBUG_ARG=""
+fi
 
-#cp /home/shiro/.rustup/toolchains/stage1/lib/rustlib/thumbv7m-none-eabi/lib/libcore-*.rlib librust_core.a
-#cp /home/shiro/.rustup/toolchains/stage1/lib/rustlib/thumbv7m-none-eabi/lib/liballoc-*.rlib librust_alloc.a
-#cp /home/shiro/.rustup/toolchains/stage1/lib/rustlib/thumbv7m-none-eabi/lib/librustc_std_workspace_core-*.rlib librustc_std_workspace_core.a
-
-
-#ar d librust_app.a $(ar t librust_app.a | grep compiler_builtins)
+echo "Buidling the image ..."
 make
-qemu-system-arm -machine mps2-an385 -cpu cortex-m3 -kernel output/RTOSDemo.out -monitor none -nographic -serial stdio -s
+
+echo "Running using Qemu. $DEBUG_MSG"
+qemu-system-arm -machine mps2-an385 -cpu cortex-m3 -kernel output/RTOSDemo.out -monitor none -nographic -serial stdio -s $DEBUG_ARG
